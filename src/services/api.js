5902b22c3c8387584852f71db4a2a4d3fd3880a3
Elias Aitovaara <elias.aitovaara@gmail.com>
2025-03-10 19:44:13 +0200

    initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "2739223e8c520c2cf114dfbde9f7ddc1";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
